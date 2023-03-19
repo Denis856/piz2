bcb4c199bef999bdb5801769c1ec012be8ce43f5
Den89 <malyhindenis8@gmail.com>
2023-03-19 18:09:41 +0500

    jjj

--- js/main.js
+++ js/main.js
@@ -13,3 +13,14 @@
     $(window).scroll(function() {
       consoleBG();
     });
+
+
+
+    
+  $(function(){
+    $('a[href^="#"]').click(function(){
+    var target = $(this).attr('href');
+    $('html, body').animate({scrollTop: $(target).offset().top}, 800);//800 - длительность скроллинга в мс
+    return false;
+    });
+    });
