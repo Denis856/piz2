002acb3ff3c71649b863fad3040babf9b4acf067
Den89 <malyhindenis8@gmail.com>
2023-05-10 18:26:24 +0300

    oo

--- js/header.js
+++ js/header.js
@@ -1,7 +1,9 @@
 var burger = document.querySelector('.header__burger');
 var menu = document.querySelector('.header__list');
+var body = document.querySelector('body');
 
 burger.onclick = () => {
    burger.classList.toggle('active');
    menu.classList.toggle('active');
+   body.classList.toggle('active');
 };
